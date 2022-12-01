0d259b512da8f6e9a58666afce73f0ced560c0ca
Zetsuboushii <grasser.luke@gmail.com>
2022-12-01 16:47:40 +0100

    Further Migration

--- repo/game.js
+++ repo/game.js
@@ -1,4 +1,5 @@
 const db = require("./db")
+const {query} = require("express");
 
 class GameRepo {
 
@@ -9,6 +10,15 @@ class GameRepo {
     static getRecommends(cb) {
         db.all("SELECT gNo, title FROM game ORDER BY RANDOM() LIMIT 10", cb)
     }
+
+    static getCurrents(cb) {
+        db.all("SELECT gNo, title FROM game ORDER BY releaseDate DESC LIMIT 10", cb)
+    }
+
+    static getCurrentBest(cb) {
+        db.all("SELECT g.gNo, g.title, p.pNo, p.name FROM game g, platform p WHERE g.releaseDate IS NOT NULL AND g.orgPlatform = p.pNo AND g.releaseDate < DATE() AND (julianday(DATE('now')) - julianday(g.releaseDate)) < 600 ORDER BY g.metaScore DESC LIMIT 3", cb)
+    }
+
 }
 
 module.exports = GameRepo
