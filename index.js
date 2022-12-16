ebd54fafacf8fac1b1c6640f0ac8bcb11cd52997
Zetsuboushii <grasser.luke@gmail.com>
2022-12-16 22:02:26 +0100

    Async Shit works now, ig

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Library's
 const express = require('express')
 const pug = require('pug')
+const path = require("path");
+
+path.join(__dirname, "./template")
+path.join(__dirname, "./repo")
+path.join(__dirname, "./public")
+path.join(__dirname, "./template")
 
 // Repos
 const GameRepo = require("./repo/game")
 
 // Init
 const app = express()
+const navbarTemp = pug.compileFile("./template/navbar.pug")
 const homeTemp = pug.compileFile("./template/home.pug")
-const hotbarTemp = pug.compileFile("./template/hotbar.pug")
 const gameTemp = pug.compileFile("./template/game.pug")
 
 
@@ -28,153 +34,30 @@ app.get("/home", async (req, res) => {
 
         res.send(homeTemp({picks, current, currentBest, pad}))
     } catch (e) {
-        console.log("Async Res Error: /home")
+        console.log("Async Res Error or Pug Error: /home")
     }
-
-    /**
-    GameRepo.getRecommends((err, recomm) => {
-        if (err) {
-            console.log(err)
-            res.send("Random Error, ig")
-            return
-        } else if (recomm.length == 0) {
-            res.send("Empty Query")
-            return
-        } else {
-            //res.send(homeTemp({recomm, pad: (x) => String(x).padStart(4, '0')}))
-            GameRepo.getCurrents((err, curr) => {
-                if (err) {
-                    console.log(err)
-                    res.send("Random Error, ig")
-                    return
-                } else if (curr.length == 0) {
-                    res.send("Empty Query")
-                    return
-                } else {
-                    //res.send(homeTemp({recomm:recomm,curr:curr, pad:pad}))
-                    GameRepo.getCurrentBest((err, currB) => {
-                        if (err) {
-                            console.log(err)
-                            res.send("Random Error, ig")
-                        } else if (currB.length == 0) {
-                            res.send("Empty Query")
-                        } else {
-
-                        }
-                    })
-                }
-            })
-        }
-    })
-    */
 })
 
 app.get("/:gameTitle", async (req, res) => {
     let pad = (x) => String(x).padStart(4, '0')
+    let date = (x) => String(x).split("-")[2] + "." + String(x).split("-")[1] + "." + String(x).split("-")[0]
     let gameTitle = req.params.gameTitle
     try {
-        let game = await GameRepo.getGamePromise(gameTitle)
-        let developer = await GameRepo.getDeveloperPromise(gameTitle)
-        let publisher = await GameRepo.getPublisherPromise(gameTitle)
-        let genre = await GameRepo.getGenrePromise(gameTitle)
-        let mode = await GameRepo.getModePromise(gameTitle)
-
-        res.send(gameTemp({game, developer, publisher, genre, mode, pad}))
+        let games = await GameRepo.getGamePromise(gameTitle)
+        let developers = await GameRepo.getDeveloperPromise(gameTitle)
+        let publishers = await GameRepo.getPublisherPromise(gameTitle)
+        let genres = await GameRepo.getGenrePromise(gameTitle)
+        let modes = await GameRepo.getModePromise(gameTitle)
+        let prequels = await GameRepo.getPrequelPromise(gameTitle)
+        let sequels = await GameRepo.getSequelPromise(gameTitle)
+        let ports = await GameRepo.getPortPromise(gameTitle)
+        console.log(games, developers, publishers, genres, modes, prequels, sequels, ports)
+        res.send(gameTemp({games, developers, publishers, genres, modes, prequels, sequels, ports, pad, date}))
     } catch (e) {
-        console.log("Async Res Error: /:gameTitle")
+        console.log("Async Res Error or Pug Error: /:gameTitle")
     }
 })
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
-
-
-/**
- GameRepo.getGame(gameTitle, (err, game) => {
-        if (err) {
-            console.log(err)
-            res.send("Random Error, ig")
-            return
-        } else if (game.length == 0) {
-            res.send("Empty Query")
-            return
-        } else {
-            GameRepo.getDeveloper(gameTitle, (err, dev) => {
-                if (err) {
-                    console.log(err)
-                    res.send("Random Error, ig ejktgeo")
-                    return
-                } else if (dev.length == 0) {
-                    res.send("Empty Query")
-                    return
-                } else {
-                    GameRepo.getPublisher(gameTitle, (err, pub) => {
-                        if (err) {
-                            console.log(err)
-                            res.send("Random Error, ig")
-                            return
-                        } else if (pub.length == 0) {
-                            res.send("Empty Query")
-                            return
-                        } else {
-                            GameRepo.getGenre(gameTitle, (err, genre) => {
-                                if (err) {
-                                    console.log(err)
-                                    res.send("Random Error, ig")
-                                    return
-                                } else if (genre.length == 0) {
-                                    res.send("Empty Query")
-                                    return
-                                } else {
-                                    GameRepo.getMode(gameTitle, (err, mode) => {
-                                        if (err) {
-                                            console.log(err)
-                                            res.send("Random Error, ig")
-                                            return
-                                        } else if (mode.length == 0) {
-                                            res.send("Empty Query")
-                                            return
-                                        } else {
-                                            res.send(gameTemp({game:game, dev:dev, pub:pub, genre:genre, mode:mode, pad:pad}))
-                                        }
-                                    })
-                                }
-                            })
-                        }
-                    })
-                }
-            })
-        }
-    })
-})
- */
-
-/**
- app.get("/games",(req,res) => {
-    GameRepo.getGames((err,games) => {
-        if(err){
-            res.send("fuck you")
-        }else if (games.length == 0){
-            res.send("you get nothing 404")
-        }else{
-            console.log(games)
-            res.send(homeTemp({games, marcel: () => console.log("this is cursed")}))
-        }
-    })
-})
- */
-
-/**
- app.get('/:gameTitle', (req, res) => {
-    GameRepo.getGame(req.params.gameTitle, (err, row) => {
-        if (err) {
-            res.send("fuck off")
-        } else if (!row) {
-            res.send("not found you whore")
-        } else {
-            res.send(homeTemp({row}))
-        }
-    })
-})
- */
--- repo/game.js
+++ repo/game.js
@@ -3,6 +3,10 @@ const {query} = require("express");
 
 class GameRepo {
 
+    static getGame(search, cb) {
+        db.all("SELECT g.gNo, g.title, g.metaScore, g.orgTitle, g.synTitle, g.releaseDate, g.synopsis, s.name AS series, p.name AS platform, (SELECT RANK() OVER (ORDER BY metaScore DESC) FROM game WHERE releaseDate < DATE('now') ORDER BY gNo LIMIT 1 OFFSET (SELECT gNo FROM game WHERE title = ?) - 1) AS rank, (SELECT AVG(score) FROM list WHERE refGame = (SELECT gNo FROM game WHERE title = ?)) AS usrscore FROM game g, platform p, series s WHERE g.orgPlatform = p.pNo AND g.refSeries = s.sNo AND g.title = ?", search, search, search, cb)
+    }
+
     static async getGamePromise(search) {
         return new Promise((resolve, reject) => {
             this.getGame(search, (err, res) => {
@@ -15,10 +19,10 @@ class GameRepo {
         })
     }
 
-    static getGame(search, cb) {
-        db.all("SELECT g.gNo, g.title, g.metaScore, g.orgTitle, g.synTitle, g.releaseDate, g.synopsis, s.name AS series, p.name AS platform FROM game g, platform p, series s WHERE g.orgPlatform = p.pNo AND g.refSeries = s.sNo AND g.title = ?", search, cb)
-    }
 
+    static getDeveloper(search, cb) {
+        db.all("SELECT d.name AS developer FROM developer d, game g, game_developer gd WHERE gd.refGame = g.gNo AND gd.refDeveloper = d.devNo AND g.title = ?", search, cb)
+    }
 
     static async getDeveloperPromise(search) {
         return new Promise((resolve, reject) => {
@@ -32,10 +36,10 @@ class GameRepo {
         })
     }
 
-    static getDeveloper(search, cb) {
-        db.all("SELECT d.name FROM developer d, game g, game_developer gd WHERE gd.refGame = g.gNo AND gd.refDeveloper = d.devNo AND g.title = ?", search, cb)
-    }
 
+    static getPublisher(search, cb) {
+        db.all("SELECT pub.name AS publisher FROM publisher pub, game g, game_publisher gp WHERE gp.refGame = g.gNo AND gp.refPublisher = pub.pubNo AND g.title = ?", search, cb)
+    }
 
     static async getPublisherPromise(search) {
         return new Promise((resolve, reject) => {
@@ -49,10 +53,10 @@ class GameRepo {
         })
     }
 
-    static getPublisher(search, cb) {
-        db.all("SELECT pub.name FROM publisher pub, game g, game_publisher gp WHERE gp.refGame = g.gNo AND gp.refPublisher = pub.pubNo AND g.title = ?", search, cb)
-    }
 
+    static getGenre(search, cb) {
+        db.all("SELECT gen.name AS genre FROM genre gen, game g, game_genre ggen WHERE ggen.refGame = g.gNo AND ggen.refGenre = gen.genNo AND g.title = ?", search, cb)
+    }
 
     static async getGenrePromise(search) {
         return new Promise((resolve, reject) => {
@@ -66,10 +70,10 @@ class GameRepo {
         })
     }
 
-    static getGenre(search, cb) {
-        db.all("SELECT gen.name FROM genre gen, game g, game_genre ggen WHERE ggen.refGame = g.gNo AND ggen.refGenre = gen.genNo AND g.title = ?", search, cb)
-    }
 
+    static getMode(search, cb) {
+        db.all("SELECT m.name AS mode FROM mode m, game g, game_mode gm WHERE gm.refGame = g.gNo AND gm.refMode = m.modeNo AND g.title = ?", search, cb)
+    }
 
     static async getModePromise(search) {
         return new Promise((resolve, reject) => {
@@ -83,10 +87,61 @@ class GameRepo {
         })
     }
 
-    static getMode(search, cb) {
-        db.all("SELECT m.name FROM mode m, game g, game_mode gm WHERE gm.refGame = g.gNo AND gm.refMode = m.modeNo AND g.title = ?", search, cb)
+
+    static getPrequel(search, cb) {
+        db.all("SELECT g.title AS prequel FROM game g, prequel p WHERE p.refOrigin = (SELECT gNo FROM game WHERE title = ?) AND p.refPrequel = g.gNo", search, cb)
     }
 
+    static async getPrequelPromise(search) {
+        return new Promise((resolve, reject) => {
+            this.getPrequel(search, (err, res) => {
+                if (err) {
+                    resolve()
+                } else {
+                    resolve(res)
+                }
+            })
+        })
+    }
+
+
+    static getSequel(search, cb) {
+        db.all("SELECT g.title AS sequel FROM game g, prequel p WHERE p.refPrequel = (SELECT gNo FROM game WHERE title = ?) AND p.refOrigin = g.gNo", search, cb)
+    }
+
+    static async getSequelPromise(search) {
+        return new Promise((resolve, reject) => {
+            this.getSequel(search, (err, res) => {
+                if (err) {
+                    resolve(["null"])
+                } else {
+                    resolve(res)
+                }
+            })
+        })
+    }
+
+
+    static getPort(search, cb) {
+        db.all("SELECT p.name AS port FROM game g, port, platform p WHERE port.refGame = g.gNo AND port.refPlatform = p.pNo AND g.title = ?", search, cb)
+    }
+
+    static async getPortPromise(search) {
+        return new Promise((resolve, reject) => {
+            this.getPort(search, (err, res) => {
+                if (err) {
+                    resolve(["null"])
+                } else {
+                    resolve(res)
+                }
+            })
+        })
+    }
+
+
+    static getPicks(cb) {
+        db.all("SELECT gNo, title FROM game ORDER BY RANDOM() LIMIT 10", cb)
+    }
 
     static async getPicksPromise() {
         return new Promise((resolve, reject) => {
@@ -100,10 +155,10 @@ class GameRepo {
         })
     }
 
-    static getPicks(cb) {
-        db.all("SELECT gNo, title FROM game ORDER BY RANDOM() LIMIT 10", cb)
-    }
 
+    static getCurrents(cb) {
+        db.all("SELECT gNo, title FROM game ORDER BY releaseDate DESC LIMIT 10", cb)
+    }
 
     static async getCurrentsPromise() {
         return new Promise((resolve, reject) => {
@@ -117,10 +172,10 @@ class GameRepo {
         })
     }
 
-    static getCurrents(cb) {
-        db.all("SELECT gNo, title FROM game ORDER BY releaseDate DESC LIMIT 10", cb)
-    }
 
+    static getCurrentBest(cb) {
+        db.all("SELECT g.gNo, g.title, p.pNo, p.name FROM game g, platform p WHERE g.releaseDate IS NOT NULL AND g.orgPlatform = p.pNo AND g.releaseDate < DATE() AND (julianday(DATE('now')) - julianday(g.releaseDate)) < 600 ORDER BY g.metaScore DESC LIMIT 3", cb)
+    }
 
     static async getCurrentBestPromise() {
         return new Promise((resolve, reject) => {
@@ -132,10 +187,6 @@ class GameRepo {
                 }
             })
         })
-    }
-
-    static getCurrentBest(cb) {
-        db.all("SELECT g.gNo, g.title, p.pNo, p.name FROM game g, platform p WHERE g.releaseDate IS NOT NULL AND g.orgPlatform = p.pNo AND g.releaseDate < DATE() AND (julianday(DATE('now')) - julianday(g.releaseDate)) < 600 ORDER BY g.metaScore DESC LIMIT 3", cb)
     }
 }
 
